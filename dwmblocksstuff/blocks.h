fb6b658a5dc5426a93069aa070050b87aca19a2f
Delta <91746122+Deluwuta@users.noreply.github.com>
2022-07-21 22:00:46 +0000

    Add files via upload

--- dwmblocksstuff/blocks.h
+++ dwmblocksstuff/blocks.h
@@ -1,14 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-    {" 📢🔊🚨✨", "", 0, 9},
-    {" 🐧 ", "~/.config/suckless/dwmblocks/scripts/kernel.sh", 0, 1},
-    {"🔔 ", "~/.config/suckless/dwmblocks/scripts/pacups.sh", 300, 2},
-    {"💾 Disk:", "~/.config/suckless/dwmblocks/scripts/diskSpace.sh", 900, 3},
-	{"🚀 ", "~/.config/suckless/dwmblocks/scripts/memory.sh", 5, 4},
-	{"⏳", "date '+%d/%m/%Y %k:%M%p '",	30,	0},
+    {"", "", 0, 9},
+    {" ", "~/.config/suckless/dwmblocks/scripts/kernel.sh", 0, 1},
+    {" ", "~/.config/suckless/dwmblocks/scripts/diskSpace.sh", 900, 3},
+    {" ", "~/.config/suckless/dwmblocks/scripts/pacups.sh", 300, 2},
+	{" ", "~/lewifi.sh", 120, 5},
+	{" ", "~/.config/suckless/dwmblocks/scripts/clock.sh", 30, 0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = " ";
 static unsigned int delimLen = 5;
