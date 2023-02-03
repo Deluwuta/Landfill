30e5ea222717af4c883a1fabfdbe2961157ecaf0
DeltΔ <91746122+Deluwuta@users.noreply.github.com>
2023-02-03 20:41:11 +0100

    Add files via upload

---
+++ corrections.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# La idea es corregir cosas, pero no puedo hacer que ella te ame
+
+if [[ $# -ne 1 ]]; then
+  echo "`basename $0`: I just need one, FUCKING, PARAMETER, dude" 1>&2
+  exit 1
+fi
+
+objection=false
+
+detectVowel(){
+  case $chara in
+    "a")
+      echo -n "á"
+      ;;
+
+    "e")
+      echo -n "é"
+      ;;
+
+    "i")
+      echo -n "í"
+      ;;
+
+    "o")
+      echo -n "ó"
+      ;;
+
+    "u")
+      if [[ $anteriorxd == "´" ]]; then
+        echo -n "ú"
+      elif [[ $anteriorxd == "¨" ]]; then
+        echo -n "ü"
+      fi
+      ;;
+
+    "n")
+      echo -n "ñ"
+      ;;
+  esac
+}
+
+while IFS="" read -rN1 chara; do
+  if [[ "$objection" = true ]]; then
+    detectVowel
+    objection=false
+  elif [[ ($chara == "´") || ($chara == "˜") || ($chara == "¨")]]; then
+    objection=true
+    anteriorxd=$chara
+  else
+    echo -en "$chara"
+  fi
+done < $1
