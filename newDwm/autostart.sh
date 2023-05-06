d00e86263d416b6a00d6ada3869c1df1651cf7fe
DeltΔ <91746122+Deluwuta@users.noreply.github.com>
2023-05-06 08:56:30 +0200

    Add files via upload

--- newDwm/autostart.sh
+++ newDwm/autostart.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# My eyes hurst broh
 redshift -P -O 4600 &
-feh --bg-fill $HOME/Pictures/background/arknightsFeater2.png &
-# dwmblocks &
+
+feh --bg-fill $HOME/Pictures/Backgrounds/arknightsFeater2.png
+
+# Notifications kek
+killall -q dunst
+while pgrep -u $UID -x dunst >/dev/null; do
+  sleep 1
+done
+dunst &
+
+# THE BAR
+dwmblocks &
--- newDwm/config.def.h
+++ newDwm/config.def.h
@@ -5,7 +5,7 @@ static const unsigned int gappx = 6;    /* gaps between windows */
 static const unsigned int snap = 32;    /* snap pixel */
 static const int showbar = 1;           /* 0 means no bar */
 static const int topbar = 1;            /* 0 means bottom bar */
-static const char *fonts[] = {"monospace:size=12"};
+static const char *fonts[] = {"Fantasque Sans Mono:size=12"};
 static const char dmenufont[] = "monospace:size=12";
 
 static const char col_gray1[] = "#222222";
@@ -81,7 +81,8 @@ static const Key keys[] = {
 
     // App bindings
     {MODKEY, XK_Return, spawn, {.v = termcmd}},
-    {MODKEY, XK_d, spawn, {.v = dmenucmd}},
+    /* {MODKEY, XK_d, spawn, {.v = dmenucmd}}, */
+    {MODKEY, XK_d, spawn, SHCMD("rofi -modi combi -show combi -display-combi 'Rufos ~>>' -combi-modi run,drun")},
     {MODKEY, XK_b, spawn, SHCMD("brave")},
     {MODKEY, XK_f, spawn, SHCMD("nautilus")},
     {MODKEY | ShiftMask, XK_d, spawn, SHCMD("discord")},
@@ -91,6 +92,15 @@ static const Key keys[] = {
     {MODKEY, XK_k, focusstack, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
+
+    {0, XF86XK_AudioLowerVolume, spawn, SHCMD("$HOME/.config/dunst/notif_scripts/volume_notif.sh down")},
+    {0, XF86XK_AudioRaiseVolume, spawn, SHCMD("$HOME/.config/dunst/notif_scripts/volume_notif.sh up")},
+
+    {0, XF86XK_MonBrightnessDown, spawn, SHCMD("brightnessctl s 5%-")},
+    {0, XF86XK_MonBrightnessUp, spawn, SHCMD("brightnessctl s 5%+")},
+
+    {0, XF86XK_KbdBrightnessDown, spawn, SHCMD("$HOME/.config/scripts/kbdbacklight.sh down")},
+    {0, XF86XK_KbdBrightnessUp, spawn, SHCMD("$HOME/.config/scripts/kbdbacklight.sh up")},
 
     /* { MODKEY, XK_i, incnmaster, {.i = +1}}, */
     /* { MODKEY, XK_d, incnmaster, {.i = -1}}, */
@@ -114,9 +124,7 @@ static const Key keys[] = {
     /* { MODKEY | ShiftMask, XK_0, tag, {.ui = ~0}}, // Fija la focused window
      */
 
-    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3)
-        TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7)
-            TAGKEYS(XK_9, 8)
+    TAGKEYS(XK_1, 0) TAGKEYS(XK_2, 1) TAGKEYS(XK_3, 2) TAGKEYS(XK_4, 3) TAGKEYS(XK_5, 4) TAGKEYS(XK_6, 5) TAGKEYS(XK_7, 6) TAGKEYS(XK_8, 7) TAGKEYS(XK_9, 8)
 
     // Quit and reload, in that order '-'
     {MODKEY | ControlMask, XK_q, quit, {0}},
