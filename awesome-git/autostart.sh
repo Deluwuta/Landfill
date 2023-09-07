6a4a8b1adff435ad50217683d9cc7b770a117030
DeltΔ <91746122+Deluwuta@users.noreply.github.com>
2023-09-07 11:54:47 +0200

    Add files via upload

--- awesome-git/autostart.sh
+++ awesome-git/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 xrandr -s 1920x1080
-xwallpaper --zoom "$HOME/Pictures/baelz-wall1.jpg" &
+xwallpaper --zoom "$HOME/Pictures/cozy_room.png" &
 /usr/bin/emacs --daemon &
 
 /usr/lib/polkit-kde-authentication-agent-1 &
--- awesome-git/ui/wibar/wibar.lua
+++ awesome-git/ui/wibar/wibar.lua
@@ -1,12 +1,55 @@
 local awful = require("awful")
 local wibox = require("wibox")
+local gears = require("gears")
+local beautiful = require("beautiful")
 
--- {{{ Wibar
+local xresources = require("beautiful.xresources")
+local dpi = xresources.apply_dpi
+
+-- local widgets = require("ui.wibar.widgets.control_center")
+
+-- Widgets
+local separator = wibox.widget {
+  {
+    id = "separator",
+    text = " | ",
+    widget = wibox.widget.textbox,
+  },
+  bg = beautiful.wibar_bg,
+  fg = beautiful.color10,
+  widget = wibox.container.background,
+}
+
+local espacer = wibox.widget {
+  {
+    id = "espacer",
+    text = " ",
+    widget = wibox.widget.textbox,
+  },
+  bg = beautiful.transparent,
+  widget = wibox.container.background,
+}
+
 -- Keyboard map indicator and switcher
-mykeyboardlayout = awful.widget.keyboardlayout()
+local mykeyboardlayout = awful.widget.keyboardlayout()
 
 -- Create a textclock widget
-mytextclock = wibox.widget.textclock()
+local clock = wibox.widget({
+  {
+    {
+      id = "clock",
+      format = "<b>" .. "%a, %d/%m ~ %H:%M" .. "</b>",
+      refresh = 5,
+      widget = wibox.widget.textclock(),
+    },
+    bg = beautiful.wibar_bg,
+    fg = beautiful.color12,
+    widget = wibox.container.background,
+  },
+  bottom = 2,
+  color = beautiful.color4,
+  widget = wibox.container.margin,
+})
 
 screen.connect_signal("request::desktop_decoration", function(s)
     -- Each screen has its own tag table.
@@ -27,10 +70,22 @@ screen.connect_signal("request::desktop_decoration", function(s)
         }
     }
 
+
+    s.win_title = awful.widget.tasklist {
+      screen = s,
+      filter = awful.widget.tasklist.filter.focused,
+      style = { tasklist_disable_icon = true, },
+    }
+
     -- Create a taglist widget
     s.mytaglist = awful.widget.taglist {
         screen  = s,
         filter  = awful.widget.taglist.filter.all,
+        style = {
+          font = beautiful.font_name .. "Medium 11",
+          shape = gears.shape.circle,
+          -- spacing = 2,
+        },
         buttons = {
             awful.button({ }, 1, function(t) t:view_only() end),
             awful.button({ mod }, 1, function(t)
@@ -48,41 +103,49 @@ screen.connect_signal("request::desktop_decoration", function(s)
     }
 
     -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist {
-        screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = {
-            awful.button({ }, 1, function (c)
-                c:activate { context = "tasklist", action = "toggle_minimization" }
-            end),
-            awful.button({ }, 3, function() awful.menu.client_list { theme = { width = 250 } } end),
-            awful.button({ }, 4, function() awful.client.focus.byidx(-1) end),
-            awful.button({ }, 5, function() awful.client.focus.byidx( 1) end),
-        }
-    }
+    --s.mytasklist = awful.widget.tasklist {
+    --    screen  = s,
+    --    filter  = awful.widget.tasklist.filter.currenttags,
+    --    buttons = {
+    --        awful.button({ }, 1, function (c)
+    --            c:activate { context = "tasklist", action = "toggle_minimization" }
+    --        end),
+    --        awful.button({ }, 3, function() awful.menu.client_list { theme = { width = 250 } } end),
+    --        awful.button({ }, 4, function() awful.client.focus.byidx(-1) end),
+    --        awful.button({ }, 5, function() awful.client.focus.byidx( 1) end),
+    --    }
+    --}
 
     -- Create the wibox
     s.mywibox = awful.wibar {
         position = "top",
-        screen   = s,
+        screen = s,
+        height = 24,
+        margins = { top = 4, bottom = 0, left = 3, right = 3 },
+        shape = gears.shape.rounded_rect,
         widget   = {
             layout = wibox.layout.align.horizontal,
             { -- Left widgets
                 layout = wibox.layout.fixed.horizontal,
-                mylauncher,
+                espacer,
                 s.mytaglist,
-                s.mypromptbox,
+                separator,
+                s.mylayoutbox,
+                separator,
+                s.win_title,
             },
-            s.mytasklist, -- Middle widget
+            { -- Middle widgets
+                layout = wibox.layout.fixed.horizontal,
+            },
             { -- Right widgets
                 layout = wibox.layout.fixed.horizontal,
                 mykeyboardlayout,
+                separator,
+                clock,
+                separator,
                 wibox.widget.systray(),
-                mytextclock,
-                s.mylayoutbox,
+                espacer,
             },
         }
     }
 end)
-
--- }}}
