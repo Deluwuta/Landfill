5e3c48c087037f3957904800b905e0ca00079cf1
Delta <91746122+Deluwuta@users.noreply.github.com>
2023-12-31 21:48:02 +0100

    Add files via upload

--- lazynvim/lua/plugins/theme.lua
+++ lazynvim/lua/plugins/theme.lua
@@ -1,8 +1,37 @@
-return { 
+local function theme_creator(name, colorscheme, background, dependencies)
+    return {
+        name,
+        lazy = false,
+        priority = 999,
+        config = function()
+            vim.cmd("set background="..background)
+            vim.cmd("colorscheme "..colorscheme)
+        end,
+        dependencies = dependencies,
+    }
+end
+
+local nightfox = theme_creator(
     "EdenEast/nightfox.nvim",
-    lazy = false,
-    priority = 999,
-    config = function()
-    	vim.cmd("colorscheme nightfox")
-    end
-}
+    "nightfox",
+    "dark"
+)
+
+local zenbones = theme_creator(
+    "mcchrish/zenbones.nvim",
+    "zenbones",
+    "dark",
+    { "rktjmp/lush.nvim" }
+)
+
+local melange = theme_creator(
+    "savq/melange-nvim",
+    "melange",
+    "dark"
+)
+
+
+return
+    -- nightfox
+    zenbones
+    -- melange
