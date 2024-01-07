c9437c93173a795b5d2870a15026c7a21aa3eef6
Delta <91746122+Deluwuta@users.noreply.github.com>
2024-01-07 01:07:54 +0100

    Theme file
    Theme file

--- dwmProject/config.h
+++ dwmProject/config.h
@@ -11,13 +11,20 @@
 Delta's Δωμ config file. 
 */
 
+// Theme
+#include "theme.h"
+
 // Autostarting
 static const char *const autostart[] = {
 	// "alacritty", NULL,
-	"sh", "-c", "xwallpaper --zoom $HOME/Pictures/black_rock_shooter_alpha.png", NULL,
+	"sh", "-c", "xwallpaper --maximize $HOME/Pictures/huTao_shakingHands.png", NULL,
   "sh", "-c", "redshift -P -O 3200 &", NULL,
   "sh", "-c", "exec /usr/lib/polkit-kde-authentication-agent-1 &", NULL,
 	"sh", "-c", "killall -q dwmblocks; while pgrep -u $UID -x dwmblocks >/dev/null; do sleep 1; done; dwmblocks &", NULL,
+
+    // VM Specifics
+    "sh", "-c", "xrandr -s 1920x1080", NULL,
+    "sh", "-c", "setxkbmap us intl altGr dead keys", NULL,
 	NULL /* terminate */
 };
 
@@ -33,29 +40,29 @@ static const char *fonts[]         = { "FiraCode Nerd Font:size=11" };
 static const char dmenufont[]      = "FiraCode Nerd Font:size=11";
 
 /* static const char col_gray1[] = "#222222"; */
-static const char col_gray2[] = "#444444";
+/* static const char col_gray2[] = "#444444"; */
 
-static const char darkest_dark[] = "#131313";
-static const char darker_black[] = "#161616";
-static const char black[]        = "#262626";
-static const char grey[]         = "#393939";
-static const char light_grey[]   = "#525252";
-static const char soft_white[]   = "#dde1e6";
-static const char white2[]       = "#faedff";
-static const char white[]        = "#f2f4f8";
-static const char pure_white[]   = "#ffffff";
-
-static const char teal_blue[]    = "#08bdba";
-static const char blue[]         = "#33b1ff";
-static const char lighter_blue[] = "#3ddbd9";
-static const char light_blue[]   = "#82cfff";
-
-static const char green[]        = "#42be65";
-
-static const char purple[]       = "#be95ff";
-
-static const char strong_pink[]  = "#ee5396";
-static const char pink[]         = "#ff7eb6";
+/* static const char darkest_dark[] = "#131313"; */
+/* static const char darker_black[] = "#161616"; */
+/* static const char black[]        = "#262626"; */
+/* static const char grey[]         = "#393939"; */
+/* static const char light_grey[]   = "#525252"; */
+/* static const char soft_white[]   = "#dde1e6"; */
+/* static const char white2[]       = "#faedff"; */
+/* static const char white[]        = "#f2f4f8"; */
+/* static const char pure_white[]   = "#ffffff"; */
+/*  */
+/* static const char teal_blue[]    = "#08bdba"; */
+/* static const char blue[]         = "#33b1ff"; */
+/* static const char lighter_blue[] = "#3ddbd9"; */
+/* static const char light_blue[]   = "#82cfff"; */
+/*  */
+/* static const char green[]        = "#42be65"; */
+/*  */
+/* static const char purple[]       = "#be95ff"; */
+/*  */
+/* static const char strong_pink[]  = "#ee5396"; */
+/* static const char pink[]         = "#ff7eb6"; */
 
 // #faedff #eeeeee //#454545
 static const char *colors[][3]      = {
