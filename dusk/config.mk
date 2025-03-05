60d60e5f00937d2b3967b6444e36cd026018c67c
Delta <91746122+Deluwuta@users.noreply.github.com>
2025-03-05 14:43:11 +0100

    Base files for Dusk wm

---
+++ dusk/config.mk
@@ -0,0 +1,69 @@
+# dusk version
+VERSION = 1.0
+
+# Customize below to fit your system
+
+# Paths
+PREFIX = /usr/local
+MANPREFIX = ${PREFIX}/share/man
+
+PKG_CONFIG = pkg-config
+
+# Optional dependency on libxi for mouse related features
+#HAVE_LIBXI = -DHAVE_LIBXI=1
+#XINPUTLIBS = `$(PKG_CONFIG) --libs xi xfixes`
+
+# Optional dependency on fribidi for RTL languages
+#HAVE_FRIBIDI = -DHAVE_FRIBIDI=1
+#FRIBIDILIBS = `$(PKG_CONFIG) --libs fribidi`
+#FRIBIDIINC = `$(PKG_CONFIG) --cflags fribidi`
+
+# Optionally set to 1 to use key codes rather than keysyms for keybindings
+USE_KEYCODES = 0
+
+# Optional dependency on Xinerama for multi-monitor support
+XINERAMALIBS  = `$(PKG_CONFIG) --libs xinerama`
+XINERAMAFLAGS = -DXINERAMA
+
+# freetype
+# FREETYPELIBS = -lfontconfig -lXft
+FREETYPELIBS = `$(PKG_CONFIG) --libs fontconfig xft`
+FREETYPEINC = `$(PKG_CONFIG) --cflags fontconfig`
+
+# Needed for alpha and window icons
+XRENDER = `$(PKG_CONFIG) --libs xrender`
+
+## Optional dependency on dbus (used for ipc / duskc)
+HAVE_DBUS = -DHAVE_DBUS=1
+DBUSLIBS = `$(PKG_CONFIG) --libs dbus-1`
+DBUSINC = `$(PKG_CONFIG) --cflags dbus-1`
+
+# Optional dependency on yajl, needed for ipc information dumps
+YAJLLIBS = `$(PKG_CONFIG) --libs yajl`
+YAJLINC = `$(PKG_CONFIG) --cflags yajl`
+
+# Needed for window swallowing functionality
+XCBLIBS = `$(PKG_CONFIG) --libs x11-xcb xcb-res`
+
+# Needed for window icons
+IMLIB2LIBS = `$(PKG_CONFIG) --libs imlib2`
+
+# Includes and libs
+INCS = ${FREETYPEINC} ${YAJLINC} ${DBUSINC} ${XINPUTINC} ${FRIBIDIINC}
+LIBS = ${XINERAMALIBS} ${FREETYPELIBS} ${XRENDER} ${XCBLIBS} ${KVMLIB} ${YAJLLIBS} ${DBUSLIBS} ${IMLIB2LIBS} ${XINPUTLIBS} ${FRIBIDILIBS}
+
+# Optional host flag for computer specific configuration
+#HOSTFLAGS = -DHOST=$(shell command -v cksum > /dev/null && hostname | cksum | tr -d ' ')
+
+# Optional compiler optimisations may create smaller binaries and
+# faster code, but increases compile time.
+# See https://gcc.gnu.org/onlinedocs/gcc/Optimize-Options.html
+#OPTIMISATIONS = -march=native -flto=auto -O3
+
+# flags
+CPPFLAGS = -D_DEFAULT_SOURCE ${HAVE_LIBXI} ${HAVE_FRIBIDI} ${HAVE_DBUS} -D_BSD_SOURCE -D_POSIX_C_SOURCE=200809L -DUSE_KEYCODES=${USE_KEYCODES} -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS} ${HOSTFLAGS}
+CFLAGS   = ${OPTIMISATIONS} -std=c99 -pedantic -Wall -Wno-unused-function -Wno-deprecated-declarations ${INCS} ${CPPFLAGS}
+LDFLAGS  = ${LIBS}
+
+# Compiler and linker
+CC = cc
---
+++ dusk/util.c
@@ -0,0 +1,136 @@
+/* See LICENSE file for copyright and license details. */
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "util.h"
+
+uint64_t settings = 0;
+
+void
+die(const char *fmt, ...)
+{
+	va_list ap;
+
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	va_end(ap);
+
+	if (fmt[0] && fmt[strlen(fmt)-1] == ':') {
+		fputc(' ', stderr);
+		perror(NULL);
+	} else {
+		fputc('\n', stderr);
+	}
+
+	exit(1);
+}
+
+void *
+ecalloc(size_t nmemb, size_t size)
+{
+	void *p;
+
+	if (!(p = calloc(nmemb, size)))
+		die("calloc:");
+	return p;
+}
+
+int
+enabled(const uint64_t functionality)
+{
+	return (settings & functionality) > 0;
+}
+
+int
+disabled(const uint64_t functionality)
+{
+	return !(settings & functionality);
+}
+
+void
+enablefunc(const uint64_t functionality)
+{
+	settings |= functionality;
+}
+
+void
+disablefunc(const uint64_t functionality)
+{
+	settings &= ~functionality;
+}
+
+void
+togglefunc(const uint64_t functionality)
+{
+	settings ^= functionality;
+}
+
+/*
+ * Copy string src to buffer dst of size dsize.  At most dsize-1
+ * chars will be copied.  Always NUL terminates (unless dsize == 0).
+ * Returns strlen(src); if retval >= dsize, truncation occurred.
+ *
+ * From:
+ * https://github.com/freebsd/freebsd-src/blob/master/sys/libkern/strlcpy.c
+ */
+size_t
+strlcpy(char * __restrict dst, const char * __restrict src, size_t dsize)
+{
+	const char *osrc = src;
+	size_t nleft = dsize;
+
+	/* Copy as many bytes as will fit. */
+	if (nleft != 0) {
+		while (--nleft != 0) {
+			if ((*dst++ = *src++) == '\0')
+				break;
+		}
+	}
+
+	/* Not enough room in dst, add NUL and traverse rest of src. */
+	if (nleft == 0) {
+		if (dsize != 0)
+			*dst = '\0';		/* NUL-terminate dst */
+		while (*src++)
+			;
+	}
+
+	return (src - osrc - 1);	/* count does not include NUL */
+}
+
+/*
+ * Appends src to string dst of size siz (unlike strncat, siz is the
+ * full size of dst, not space left).  At most siz-1 characters
+ * will be copied.  Always NUL terminates (unless siz <= strlen(dst)).
+ * Returns strlen(src) + MIN(siz, strlen(initial dst)).
+ * If retval >= siz, truncation occurred.
+ */
+size_t
+strlcat(char *dst, const char *src, size_t siz)
+{
+	char *d = dst;
+	const char *s = src;
+	size_t n = siz;
+	size_t dlen;
+
+	/* Find the end of dst and adjust bytes left but don't go past end */
+	while (n-- != 0 && *d != '\0')
+		d++;
+	dlen = d - dst;
+	n = siz - dlen;
+
+	if (n == 0)
+		return(dlen + strlen(s));
+	while (*s != '\0') {
+		if (n != 1) {
+			*d++ = *s;
+			n--;
+		}
+		s++;
+	}
+	*d = '\0';
+
+	return(dlen + (s - src));   /* count does not include NUL */
+}
