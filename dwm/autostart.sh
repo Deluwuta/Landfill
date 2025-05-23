dfb1c604ee9d9811db00bf0ad6d03bc8f92c08f0
Delta <91746122+deltarch-cmd@users.noreply.github.com>
2025-05-23 20:00:14 +0200

    Dwm build
    Removed cool-autostart and rainbowtags patches.
    Autostarting is done by a simple function in dwm.c and the autostart.sh file.
    Trying out pywal right now, but there is not much overhead in the build.

---
+++ dwm/autostart.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Idk what this if for (dwm)
+xset s off
+xset s noblank
+xset -dpms
+
+# VM's
+xrandr -s 1920x1080
+sleep 0.2
+setxbkmap us intl altGr
+
+redshift -P -O 3000 &
+nm-applet &
+
+# feh --bg-fill $HOME/Pictures/irene_arknights.jpg
+wal -R
+
+# Launch dwmblocks
+killall -q dwmblocks
+while pgrep -u $UID -x dwmblocks >/dev/null; do 
+    sleep 0.2
+done
+dwmblocks &
---
+++ dwm/themes/tokyonight.h
@@ -0,0 +1,9 @@
+static const char red[] = "";
+static const char alt_red[] = "";
+
+static const char orange[] = "";
+static const char alt_orange[] = "";
+
+static const char blue[] = "";
+static const char green[] = "";
+static const char pink[] = "";
